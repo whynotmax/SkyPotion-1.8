d43fbfabfef352c9fe5a682be5b96af8f8c641d0
Keine Secrets <miethe.max@web.de>
2024-09-18 13:36:22 +0200

    Registered DatabaseManager in PotionPlugin

--- src/main/java/eu/skypotion/PotionPlugin.java
+++ src/main/java/eu/skypotion/PotionPlugin.java
@@ -1,11 +1,15 @@
 package eu.skypotion;
 
+import eu.skypotion.mongo.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PotionPlugin extends JavaPlugin {
 
+    DatabaseManager databaseManager;
+
     @Override
     public void onEnable() {
+        databaseManager = new DatabaseManager();
         // Plugin startup logic
 
     }
