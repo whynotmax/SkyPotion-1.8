a0d63a1d06eb9a0e7d0314779d0e1cc083fa6e69
Keine Secrets <miethe.max@web.de>
2024-09-18 12:04:15 +0200

    Added ProjectConstants#PREFIX and more like this

---
+++ src/main/java/eu/skypotion/ProjectConstants.java
@@ -0,0 +1,12 @@
+package eu.skypotion;
+
+public class ProjectConstants {
+
+    public static final String PREFIX = "§8» §5§lSkyPotion§8 ┃ §7";
+    public static final String NO_PERMISSION = PREFIX + "§cDazu hast du keine Rechte§8! (§c%s§8)";
+    public static final String PLAYER_NOT_FOUND = PREFIX + "§cDer Spieler %s wurde nicht gefunden§8!";
+    public static final String PLAYER_NOT_ONLINE = PREFIX + "§cDer Spieler %s ist nicht online§8!";
+    public static final String INVALID_USAGE = PREFIX + "§cVerwendung: %s";
+    public static final String INVALID_NUMBER = PREFIX + "§cBitte gebe eine gültige Zahl an§8!";
+
+}
