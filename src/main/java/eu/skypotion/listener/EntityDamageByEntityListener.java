44aeb14a5187fbedf56ee213d8ee5b28d9abc3b4
Keine Secrets <miethe.max@web.de>
2024-09-22 20:53:26 +0200

    Added CombatLog and EntityDamageByEntityListener

---
+++ src/main/java/eu/skypotion/listener/EntityDamageByEntityListener.java
@@ -0,0 +1,37 @@
+package eu.skypotion.listener;
+
+import eu.skypotion.ProjectConstants;
+import eu.skypotion.util.combat.CombatLog;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+public class EntityDamageByEntityListener implements Listener {
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player damager && event.getEntity() instanceof Player damaged) {
+            if (damager.equals(damaged)) {
+                event.setCancelled(true);
+                damager.sendMessage(ProjectConstants.PREFIX + "§cDu kannst dir selbst keinen Schaden zufügen.");
+                return;
+            }
+            CombatLog.addToCombatLog(damager.getUniqueId(), damaged.getUniqueId());
+            return;
+        }
+        if (event.getDamager() instanceof Projectile projectile && projectile.getShooter() instanceof Player damager && event.getEntity() instanceof Player damaged) {
+            if (damager.equals(damaged)) {
+                event.setCancelled(true);
+                damager.sendMessage(ProjectConstants.PREFIX + "§cDu kannst dir selbst keinen Schaden zufügen.");
+                return;
+            }
+            CombatLog.addToCombatLog(damager.getUniqueId(), damaged.getUniqueId());
+        }
+    }
+
+}
---
+++ src/main/java/eu/skypotion/util/combat/CombatLog.java
@@ -0,0 +1,91 @@
+package eu.skypotion.util.combat;
+
+import eu.skypotion.PotionPlugin;
+import eu.skypotion.ProjectConstants;
+import eu.skypotion.util.ActionBar;
+import eu.skypotion.util.TimeUtil;
+import lombok.experimental.UtilityClass;
+import net.md_5.bungee.api.ChatMessageType;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+@UtilityClass
+public class CombatLog {
+
+    private static PotionPlugin plugin;
+
+    public static void init(PotionPlugin plugin) {
+        CombatLog.plugin = plugin;
+    }
+
+    private final Map<UUID, Long> combatLogTime = new ConcurrentHashMap<>();
+    private final Map<UUID, UUID> combatLog = new ConcurrentHashMap<>();
+    private final Map<UUID, BukkitTask> tasks = new ConcurrentHashMap<>();
+
+    public void addToCombatLog(UUID attacker, UUID victim) {
+        long endTime = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(15);
+        combatLogTime.put(attacker, endTime);
+        combatLogTime.put(victim, endTime);
+
+        combatLog.put(attacker, victim);
+        combatLog.put(victim, attacker);
+
+        tasks.put(attacker, Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            Player player = Bukkit.getPlayer(attacker);
+            if (player != null) {
+                ActionBar.sendActionBar(player, "§7Gegner§8: §c" + Bukkit.getPlayer(combatLog.get(attacker)).getName() + " §8┃ §7Verbleibend§8: §c" + TimeUtil.beautifyTime(getTimeRemaining(attacker), TimeUnit.MILLISECONDS) + "s");
+            }
+            if (getTimeRemaining(attacker) == 0) {
+                removeFromCombatLog(attacker);
+                if (player != null) player.sendMessage(ProjectConstants.PREFIX + "§cDu bist nun nicht mehr im Kampf.");
+                tasks.get(attacker).cancel();
+            }
+        }, 0L, 10L));
+
+        tasks.put(victim, Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            Player player = Bukkit.getPlayer(victim);
+            if (player != null) {
+                ActionBar.sendActionBar(player, "§7Gegner§8: §c" + Bukkit.getPlayer(combatLog.get(victim)).getName() + " §8┃ §7Verbleibend§8: §c" + TimeUtil.beautifyTime(getTimeRemaining(victim), TimeUnit.MILLISECONDS) + "s");
+            }
+            if (getTimeRemaining(victim) == 0) {
+                removeFromCombatLog(victim);
+                if (player != null) player.sendMessage(ProjectConstants.PREFIX + "§cDu bist nun nicht mehr im Kampf.");
+            }
+        }, 0L, 10L));
+    }
+
+    public long getTimeRemaining(UUID player) {
+        Long endTime = combatLogTime.get(player);
+        if (endTime == null) {
+            return 0;
+        }
+        long timeRemaining = endTime - System.currentTimeMillis();
+        return timeRemaining > 0 ? timeRemaining : 0;
+    }
+
+    public boolean isInCombat(UUID player) {
+        return combatLogTime.containsKey(player);
+    }
+
+    public UUID getOpponent(UUID player) {
+        return combatLog.get(player);
+    }
+
+    public void removeFromCombatLog(UUID player) {
+        combatLogTime.remove(player);
+        combatLog.remove(player);
+
+        BukkitTask task = tasks.getOrDefault(player, null);
+        if (task != null) {
+            task.cancel();
+            tasks.remove(player);
+        }
+    }
+
+}
