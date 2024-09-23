e4e3ef9c8c2225926f06d142436659889c49dd09
Keine Secrets <miethe.max@web.de>
2024-09-23 14:22:18 +0200

    Implemented CrateRepository in DatabaseManager

--- src/main/java/eu/skypotion/mongo/DatabaseManager.java
+++ src/main/java/eu/skypotion/mongo/DatabaseManager.java
@@ -2,6 +2,9 @@ package eu.skypotion.mongo;
 
 import eu.koboo.en2do.Credentials;
 import eu.koboo.en2do.MongoManager;
+import eu.skypotion.crates.CrateManager;
+import eu.skypotion.crates.animation.CrateAnimation;
+import eu.skypotion.crates.repository.CrateRepository;
 import eu.skypotion.mongo.player.PotionPlayerManager;
 import eu.skypotion.mongo.player.repository.PotionPlayerRepository;
 import eu.skypotion.mongo.season.SeasonManager;
@@ -20,10 +23,12 @@ public class DatabaseManager {
     SeasonRepository seasonRepository;
     PotionPlayerRepository potionPlayerRepository;
     TeleportRepository teleportRepository;
+    CrateRepository crateRepository;
 
     PotionPlayerManager potionPlayerManager;
     TeleportManager teleportManager;
     SeasonManager seasonManager;
+    CrateManager crateManager;
 
     public DatabaseManager() {
         mongoManager = new MongoManager(Credentials.of("mongodb://keinepixel:r7M3LHbAVxq9uYX5Jdn6gsSFk4DfUGt2@45.81.232.200:27017/", "nigga"));
@@ -40,10 +45,12 @@ public class DatabaseManager {
         seasonRepository = mongoManager.create(SeasonRepository.class);
         potionPlayerRepository = mongoManager.create(PotionPlayerRepository.class);
         teleportRepository = mongoManager.create(TeleportRepository.class);
+        crateRepository = mongoManager.create(CrateRepository.class);
 
         potionPlayerManager = new PotionPlayerManager(potionPlayerRepository);
         seasonManager = new SeasonManager(seasonRepository);
         teleportManager = new TeleportManager(teleportRepository);
+        crateManager = new CrateManager(crateRepository);
     }
 
 }
